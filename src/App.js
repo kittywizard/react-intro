a90da24677c2608787595ce8519bdeffe2f9071f
marleigh <volcana@gmail.com>
2021-11-12 11:16:02 -0800

    adding basic info to page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="react logo" className="img"></img>
 
+      <h3>Fun facts about React</h3>
+      <ul>
+        <li> Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has over 100k stars on Github</li>
+        <li>Maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile</li>
+      </ul>
     </div>
   );
 }
